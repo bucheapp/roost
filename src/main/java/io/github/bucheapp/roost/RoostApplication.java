f65ea3c08e47d065e2b0db32cb5507d376b3c3fc
bucheapp <bucheapp@gmail.com>
2026-02-08 04:36:14 +0900

    initial commit

---
+++ src/main/java/io/github/bucheapp/roost/RoostApplication.java
@@ -0,0 +1,12 @@
+package io.github.bucheapp.roost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoostApplication.class, args);
+	}
+}
---
+++ src/test/java/io/github/bucheapp/roost/RoostApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bucheapp.roost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
